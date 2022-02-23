a21c7cd35dc4761279de688bc4691cceac70c362
taroodr <n.kotaro.0101@gmail.com>
2022-02-23 18:21:38 +0900

    feat: add esa extension

---
+++ src/hooks/useSearch.tsx
@@ -0,0 +1,70 @@
+import { getPreferenceValues } from "@raycast/api";
+import fetch from "node-fetch";
+import { useState } from "react";
+import useSWR from "swr";
+
+type Post = {
+  number: number;
+  name: string;
+  full_name: string;
+  wip: boolean;
+  body_md: string;
+  body_html: string;
+  created_at: string;
+  message: string;
+  url: string;
+  updated_at: string;
+  tags: string[];
+  category: string;
+  revision_number: number;
+  created_by: {
+    myself: boolean;
+    name: string;
+    screen_name: string;
+    icon: string;
+  };
+  updated_by: {
+    myself: boolean;
+    name: string;
+    screen_name: string;
+    icon: string;
+  };
+};
+
+type Posts = {
+  posts: Post[];
+};
+
+interface Preferences {
+  apiKey: string;
+  teamName: string;
+}
+
+const buildFetcher = ({ teamName, apiKey, searchText }: { teamName: string; apiKey: string; searchText: string }) => {
+  return (): Promise<Posts> =>
+    fetch(`https://api.esa.io/v1/teams/${teamName}/posts?q=${searchText}&sort=best_match`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+      method: "GET",
+      // 型はあとで頑張る
+    }).then((res) => res.json() as Promise<Posts>);
+};
+
+export const useSearch = () => {
+  const { apiKey, teamName } = getPreferenceValues<Preferences>();
+  const [searchText, setSearchText] = useState("");
+
+  if (!apiKey) throw new Error("Missing API key.");
+  if (!teamName) throw new Error("Missing team name.");
+
+  const fetcher = buildFetcher({ teamName, apiKey, searchText });
+
+  const { data, error } = useSWR<Posts>(`https://api.esa.io/v1/teams/:team_name/posts?q=${searchText}`, fetcher);
+
+  return {
+    data,
+    error,
+    setSearchText,
+  };
+};
---
+++ src/search.tsx
@@ -0,0 +1,52 @@
+import { ActionPanel, Detail, List, Action, showToast, Toast } from "@raycast/api";
+import { useSearch } from "./hooks/useSearch";
+
+export default function Command() {
+  const { data, setSearchText, error } = useSearch();
+
+  if (error) {
+    showToast({ style: Toast.Style.Failure, title: error.message });
+    return;
+  }
+
+  return (
+    <List
+      onSearchTextChange={(text: string) => {
+        console.log(text);
+        setSearchText(text);
+      }}
+      searchBarPlaceholder="Search posts..."
+      throttle
+      isLoading={data == null || error}
+    >
+      {data?.posts?.map((post) => (
+        <List.Item
+          icon="list-icon.png"
+          key={post.number}
+          title={post.name}
+          subtitle={post.full_name}
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser
+                url={post.url}
+                icon="link-icon.png"
+                shortcut={{
+                  key: "o",
+                  modifiers: ["cmd"],
+                }}
+              />
+              <Action.Push
+                title="Show Details"
+                shortcut={{
+                  key: "d",
+                  modifiers: ["cmd"],
+                }}
+                target={<Detail markdown={post.body_md} />}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
